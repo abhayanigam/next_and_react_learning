8c41ea298034e566c572fdee1ddf457c28028048
abhayanigam <abhaya.nigam13@gmail.com>
2025-03-31 23:25:00 +0530

    Added next redux utut

---
+++ next-redux-toolkit-app/app/components/Counter.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../redux/store";
+import { increment, decrement, incrementByAmount } from "../slices/counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>Increment by 5</button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ next-redux-toolkit-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Counter from "./components/Counter";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Redux Toolkit in Next.js</h1>
+      <Counter />
+    </div>
+  );
+}
